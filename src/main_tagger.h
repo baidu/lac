47a7d21eb8a5a20cbfc77bdd3d02bdf0255d4af3
Wojciech Uss <wojciech.uss@intel.com>
2018-08-23 15:34:33 +0200

    fix include path for init.h

--- src/main_tagger.h
+++ src/main_tagger.h
@@ -18,7 +18,7 @@ limitations under the License. */
 #include <set>
 #include <vector>
 #include "lac_glb.h"
-#include "paddle/fluid/framework/init.h"
+#include "paddle/fluid/platform/init.h"
 #include "paddle/fluid/inference/io.h"
 
 namespace lac {
