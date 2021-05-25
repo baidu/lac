3e10dbed9bfd87bea927c84a6627a167c17b5617
huangdingbang <huangdingbang@baidu.com>
2021-05-25 18:46:04 +0800

    fix description failed to render in the default format of reStructuredText

--- python/setup.py
+++ python/setup.py
@@ -51,7 +51,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setup(
     name="LAC",
-    version="2.1.0",
+    version="2.1.2",
     author="Baidu NLP",
     author_email="nlp-fenci@baidu.com",
     description="A chinese lexical analysis tool by Baidu NLP.",
